7dfe5f82947a167d94720def9969c40f148ad235
chenwei <@chenwei>
2023-02-21 09:53:09 +0800

    test提交

---
+++ src/main/java/com/example/gitdemon/GitDemonApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitdemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemonApplication.class, args);
+    }
+
+}
